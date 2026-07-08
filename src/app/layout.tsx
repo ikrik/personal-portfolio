9e574ce16ab5a2f6b0a4cefac564f5a66cd85d8f
Krik Tsakma <gregtsakma@gmail.com>
2026-07-08 20:38:10 +0300

    chore(app): add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 
@@ -65,6 +66,7 @@ export default function RootLayout({
     >
       <body className="flex min-h-full flex-col">{children}</body>
       <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
